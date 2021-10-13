53b4217ac1ff95c0848df45275e131719e280f68
David <58850140+Xuplesia@users.noreply.github.com>
2021-10-13 21:28:48 +1300

    Update HTML, CSS and JavaScript
    Updated the app to display a 'words per minute' value when the test finishes, and a reset button that can be toggled to.
    
    Calculating the WPM has been left to a separate function that iterates through the words array at the end, calculating results based on the classes added to each letter during the test, rather than incrementing the values inside the character checks.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // setup variables
-const wordsContainer = document.querySelector('.words-container');
 const wordsInput = document.querySelector('.words-input');
 const wordsDisplay = document.querySelector('.words-display');
+const resetButton = document.querySelector('.reset-button');
+const scoreContainer = document.querySelector('.score-container');
 
 const wordList = "Hello how Yes are you i'm good what about you yes very great nice to see because limited time against clock";
 
@@ -13,6 +14,7 @@ let wordsArray;
 let letterCounter;
 let wordCounter;
 let startTime;
+let endTime;
 
 let correctLetters;
 let incorrectLetters;
@@ -24,6 +26,37 @@ let missedLetters;
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
+}
+
+function wpmCalc(startTime, endTime, wordsArray) {
+  let totalCharacters = 0;
+  let correctLetters = 0;
+  let incorrectLetters = 0;
+  let extraLetters = 0;
+  let missedLetters = 0;
+
+  for (let i = 0; i < wordsArray.length; i++) {
+    for (let j = 0; j < wordsArray[i].length; j++) {
+      totalCharacters++;
+      if (wordsArray[i][j].classList.contains('correct')) {
+        correctLetters++;
+      } else if (wordsArray[i][j].classList.contains('incorrect')) {
+        incorrectLetters++;
+      } else if (wordsArray[i][j].classList.contains('extra-letter')) {
+        extraLetters++;
+      }
+    }
+  }
+  // add spaces to correct letters count
+  // adjust total letters count for disregared end space
+  correctLetters += words - 1;
+  totalCharacters -= 1;
+  missedLetters = totalCharacters - (correctLetters + incorrectLetters + extraLetters);
+
+  let time = endTime - startTime;
+  time = time / 1000 / 60;
+  let grossWPM = ((totalCharacters - extraLetters) / 5) / time;
+  return `🎉 WPM: ${grossWPM.toFixed(0)}`;
 }
 
 
@@ -39,6 +72,16 @@ function setup() {
   letterCounter = 0;
   wordCounter = 0;
 
+  started = false;
+  finished = false;
+
+  scoreContainer.classList.remove('score-visible');
+
+  // clear existing words
+  while (wordsDisplay.firstChild) {
+    wordsDisplay.firstChild.remove();
+  }
+
   // build and display words list
   let splitWords = wordList.split(' ');
 
@@ -53,23 +96,19 @@ function setup() {
       word.appendChild(letter);
       letters.push(letter);
     }
-    // add spacing span at the end of every word but the last one
-    if (i != wordsArray.length - 1) {
-      let spacing = document.createElement('span');
-      spacing.innerText = ' ';
-      word.appendChild(spacing);
-      letters.push(spacing);
-    }
+    // add spacing span at the end of every word
+    let spacing = document.createElement('span');
+    spacing.innerText = ' ';
+    word.appendChild(spacing);
+    letters.push(spacing);
     wordsArray.push(letters);
     wordsDisplay.appendChild(word);
   }
   wordsArray[0][0].classList.add('caret');
 }
 
-
 // first time setup
 setup();
-
 
 // words input focus
 wordsDisplay.onclick = function() {
@@ -79,20 +118,53 @@ wordsInput.onblur = function() {
   wordsArray[wordCounter][letterCounter].classList.remove('caret');
 }
 wordsInput.onfocus = function() {
-  wordsArray[wordCounter][letterCounter].classList.add('caret');
+  if (!finished){
+    wordsArray[wordCounter][letterCounter].classList.add('caret');
+  }
 }
 
+// tab key focus on reset button
+window.onkeydown = function(e) {
+  if (e.code === "Tab") {
+    e.preventDefault();
+    resetButton.focus();
+  }
+}
+
+// reset button function click
+resetButton.onclick = function() {
+  // reset focus to the input box and call the setup function
+  wordsInput.focus();
+  setup();
+}
 
 // keystroke checking
 wordsInput.oninput = function(e) {
+  // check if the test is running, if not, record the time the user starts typing
+  if (!started) {
+    started = true;
+    startTime = Date.now();
+  }
+  // if the game has stopped, return
+  if (finished){
+    return;
+  }
   // letter input checking
   // set the current letter as the letter that the user has just typed
   let currentLetter = e.data;
-  // console.log(currentLetter); // Debugging keystroke
   // space input, checking if the user has entered at least one letter in the current word
   if (currentLetter === ' ' && letterCounter > 0) {
-    // move caret to the next word and set letter selector back to zero
+    // move caret to the next word and set letter selector back to zero, if the word is the last one, end the game
     wordsArray[wordCounter][letterCounter].classList.remove('caret');
+    // end the test if the current word is the last one
+    if (wordCounter === wordsArray.length - 1) {
+      finished = true;
+      endTime = Date.now();
+      scoreContainer.firstElementChild.innerText = wpmCalc(startTime, endTime, wordsArray);
+      scoreContainer.classList.add('score-visible');
+      return;
+    }
+    // otherwise contiunue and move caret to the next word and check previous for errors
     wordCounter++;
     letterCounter = 0;
     wordsArray[wordCounter][letterCounter].classList.add('caret');
@@ -107,11 +179,19 @@ wordsInput.oninput = function(e) {
     // clear input box
     wordsInput.value = '';
   } else if (currentLetter === wordsArray[wordCounter][letterCounter].innerText && currentLetter !== ' ' && currentLetter !== null) {
-    // checking if correct letter has been entered, if so, move caret forward and style test accordingly
+    // checking if correct letter has been entered, if so, move caret forward and style test accordingly, and check if it was the last character, ending the test
     wordsArray[wordCounter][letterCounter].classList.add('correct');
-    wordsArray[wordCounter][letterCounter + 1].classList.add('caret');
     wordsArray[wordCounter][letterCounter].classList.remove('caret');
-    correctLetters++;
+    // end the test if the current letter is corrent and the last letter of the last word
+    if (letterCounter === wordsArray[wordCounter].length - 2 && wordCounter === wordsArray.length - 1) {
+      finished = true;
+      endTime = Date.now();
+      scoreContainer.firstElementChild.innerText = wpmCalc(startTime, endTime, wordsArray);
+      scoreContainer.classList.add('score-visible');
+      return;
+    }
+    // otherwise contiunue and move caret forward and increment letterCounter;
+    wordsArray[wordCounter][letterCounter + 1].classList.add('caret');
     letterCounter++;
   } else if (currentLetter !== wordsArray[wordCounter][letterCounter].innerText && currentLetter !== ' ' && currentLetter !== null) {
     // checking if incorrect or extra letter
@@ -120,7 +200,6 @@ wordsInput.oninput = function(e) {
       wordsArray[wordCounter][letterCounter].setAttribute('typed-letter', currentLetter);
       wordsArray[wordCounter][letterCounter + 1].classList.add('caret');
       wordsArray[wordCounter][letterCounter].classList.remove('caret');
-      incorrectLetters++;
       letterCounter++;
     } else if (wordsArray[wordCounter].length < 20) {
       // insert extra word
@@ -129,7 +208,6 @@ wordsInput.oninput = function(e) {
       extraLetter.classList.add('extra-letter');
       wordsArray[wordCounter][letterCounter].parentNode.insertBefore(extraLetter, wordsArray[wordCounter][letterCounter]);
       wordsArray[wordCounter].splice(letterCounter, 0, extraLetter);
-      // count extra
       letterCounter++;
     } else {
       // if too many extra letters have been added already, don't add anymore, and remove further extras from input box
@@ -139,9 +217,13 @@ wordsInput.oninput = function(e) {
 }
 
 wordsInput.onkeydown = function(e) {
+  // if the game has stopped, return
+  if (finished){
+    return;
+  }
   // backspace input
   if (e.code === 'Backspace') {
-    if (letterCounter != 0) {
+    if (letterCounter !== 0) {
       // remove styling from incorrect letter, or remove extra letter
       if (!wordsArray[wordCounter][letterCounter - 1].classList.contains('extra-letter')) {
         wordsArray[wordCounter][letterCounter - 1].classList.remove('correct');
@@ -153,14 +235,13 @@ wordsInput.onkeydown = function(e) {
       } else {
         // if letter is an extra added to the end of the word, remove it
         if (wordsArray[wordCounter][letterCounter - 1].classList.contains('extra-letter')) {
-          // remove extra letter
+          // remove extra letter and decrement the letters counter
           wordsArray[wordCounter][letterCounter - 1].remove();
           wordsArray[wordCounter].splice(letterCounter - 1, 1);
-          // move caret foward and count extra
           letterCounter--;
         }
       }
-    } else {
+    } else if (wordCounter !== 0) {
       // return to the previous word if it contains incorrect lettering, or added letters
       // prevent default behaviour of backspacing the inputbox after adding the previous word in,
       // otherwise there would always be a letter at the end missing
